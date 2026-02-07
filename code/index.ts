c1df1e0b2fc7864d1408984af1ae78db79d833ca
lebrancconvas <lebrancconvas@gmail.com>
2026-02-08 01:22:52 +0700

    ✨feat: update codes.

--- code/index.ts
+++ code/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+import { P, and, or, imply, iff } from "./libs/logic";
+
+function main() {
+  const t = new P("T");
+  const f = new P("F");
+  console.log(and(t, f));
+}
+
+main();
